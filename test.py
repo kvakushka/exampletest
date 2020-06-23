b5e69f15c2c9118ca73b178b0065b890f12ec3a2
kvakushka <58634097+kvakushka@users.noreply.github.com>
2020-06-23 21:22:49 +0300

    туцтуц

--- test.py
+++ test.py
@@ -1,8 +1,15 @@
 import os
-import pandas as pd
+import pandas as pd #импортируем все нужные модули
 import fnmatch
 
-for file in os.listdir('.'):
-    if fnmatch.fnmatch(file, '*.xlsx'):
-        df = pd.read_excel(file)
-        df = df.to_excel('result.xls', engine = 'xlsxwriter', index = False)
+begin_path = r'C:\example' #прописываем СВОЙ путь к папке, в которой у нас хранится .xlsx
+files_in_path = os.listdir(begin_path) #создаем список из элементов, которые есть в указанном пути
+for file in files_in_path: #создаём цикл для поиска файлов нужного нам формата
+    if fnmatch.fnmatch(file, '*.xlsx'): #проверяем, что за .xlsx есть
+        df = pd.read_excel(file) #читаем файл с помощью пандаса
+        yourdir = r'C:\example' #прописываем СВОЙ путь, в который хотим записать .xls
+        final_name = r'\result.xls' #имя финального .xls
+        final_path = yourdir + final_name #прописываем путь к папке, в которую хотим сохранить данные в .xls, суммируем строки
+        writer = pd.ExcelWriter(final_path , engine='xlsxwriter') #записываем данные в файл в указанном пути
+        df = df.to_excel(writer, index = False) #форматируем датафрейм в xls, убираем столбец индексов
+        writer.save() #сохраняем результат
